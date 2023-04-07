cfe75d0f491b50508a0161d444a16adc23f0503c
Klutrem <danyaryabov@mail.ru>
2023-04-07 15:22:46 +0300

    added env variables

--- docker-nginx-tutorial.tf
+++ docker-nginx-tutorial.tf
@@ -7,8 +7,17 @@ terraform {
   }
 }
 
+variable "MONGO_INITDB_ROOT_USERNAME" {
+  type        = string
+  description = "This is an example input variable using env variables."
+  default     = "admin"
+}
+variable "MONGO_INITDB_ROOT_PASSWORD" {
+  type        = string
+  description = "This is an example input variable using env variables."
+  default     = "admin"
+}
 provider "docker" {}
-
 resource "docker_image" "mongo" {
   name = "mongo:latest"
 }
@@ -16,6 +25,7 @@ resource "docker_image" "mongo" {
 resource "docker_container" "mongo" {
   image = docker_image.mongo.image_id
   name  = "mongodb"
+  env   = ["MONGO_INITDB_ROOT_PASSWORD=admin", "MONGO_INITDB_ROOT_USERNAME=admin"]
 
   ports {
     internal = 27017
