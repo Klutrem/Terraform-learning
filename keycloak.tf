62d13494ff12b9a9959c8613f933e64bee0b1d84
Klutrem <danyaryabov@mail.ru>
2023-04-14 19:12:22 +0300

    keycloak and postgres credentials are now in secret

--- keycloak.tf
+++ keycloak.tf
@@ -13,45 +13,42 @@ resource "helm_release" "keycloak" {
 
   set {
     name  = "postgresql.auth.postgresPassword"
-    value = "admin"
+    value = kubernetes_secret.keycloak-postgresql1.data["POSTGRES_POSTGRES_PASSWORD"]
   }
   set {
     name  = "postgresql.auth.password"
-    value = "admin"
+    value = kubernetes_secret.keycloak-postgresql1.data["POSTGRES_PASSWORD"]
   }
   set {
     name  = "auth.adminPassword"
-    value = "admin"
+    value = kubernetes_secret.keycloak1.data["KEYCLOAK_ADMIN_PASSWORD"]
   }
   set {
     name  = "auth.adminUser"
-    value = "admin"
+    value = kubernetes_secret.keycloak1.data["KEYCLOAK_ADMIN"]
+  }
+
+}
+
+resource "kubernetes_secret" "keycloak-postgresql1" {
+  metadata {
+    name      = "keycloak-postgresql1"
+    namespace = var.namespace
+  }
+  data = {
+    POSTGRES_POSTGRES_PASSWORD = "admin"
+    POSTGRES_PASSWORD          = "admin"
   }
 }
 
-# resource "kubernetes_secret" "fuck-keycloak-postgresql" {
-#   metadata {
-#     name      = "fuck-keycloak-postgresql"
-#     namespace = var.namespace
-#   }
-#   data = {
-#     POSTGRES_POSTGRES_PASSWORD = "admin"
-#     POSTGRES_PASSWORD          = "admin"
-#     KEYCLOAK_DATABASE_PASSWORD = "admin"
-#     postgres-password          = "admin"
-#     password                   = "admin"
-#     admin-password             = "admin"
-#   }
-# }
-
-# resource "kubernetes_secret" "fuck-keycloak" {
-#   metadata {
-#     name      = "fuck-keycloak"
-#     namespace = var.namespace
-#   }
-#   data = {
-#     KEYCLOAK_ADMIN_PASSWORD    = "admin"
-#     KEYCLOAK_DATABASE_PASSWORD = "admin"
-#     admin-password             = "admin"
-#   }
-# }
+resource "kubernetes_secret" "keycloak1" {
+  metadata {
+    name      = "keycloak1"
+    namespace = var.namespace
+  }
+  data = {
+    KEYCLOAK_ADMIN_PASSWORD    = "admin"
+    KEYCLOAK_DATABASE_PASSWORD = "admin"
+    KEYCLOAK_ADMIN             = "user"
+  }
+}
